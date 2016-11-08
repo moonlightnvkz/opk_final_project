185f9cbf777a1a76cca24ffaaf155efd87836b0a
moonlightnvkz <moonlightnvkz@users.noreply.github.com>
2016-11-08 18:52:10 +0700

    Initial commit. First try in creatin server.

---
+++ client/main.cpp
@@ -0,0 +1,64 @@
+/*
+    C ECHO client example using sockets
+*/
+#include<stdio.h> //printf
+#include<string.h>    //strlen
+#include<sys/socket.h>    //socket
+#include<arpa/inet.h> //inet_addr
+#include <stdbool.h>
+
+int main(int argc , char *argv[])
+{
+    int sock;
+    struct sockaddr_in server;
+    char message[1000] , server_reply[2000];
+
+    //Create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1) {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 8888 );
+
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0) {
+        perror("connect failed. Error");
+        return 1;
+    }
+
+    puts("Connected\n");
+
+    //keep communicating with server
+    bool exit = false;
+    while(!exit) {
+        printf("Enter message : ");
+        scanf("%s" , message);
+        if (strcmp(message, "exit") == 0) {
+            exit = true;
+        }
+        //Send some data
+        if( send(sock , message , strlen(message) , 0) < 0) {
+            puts("Send failed");
+            return 1;
+        }
+
+        //Receive a reply from the server
+        if( recv(sock , server_reply , 2000 , 0) < 0) {
+            puts("recv failed");
+            break;
+        }
+
+        puts("Server reply :");
+        puts(server_reply);
+    }
+
+
+    if (shutdown(sock, 2) == -1) {
+        perror("Faliled to close the socket");
+    };
+    return 0;
+}
